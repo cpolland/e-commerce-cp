5b1a84637e2f837174c736b35890afea455ddba2
Cody Polland <cody.polland@gmail.com>
2022-05-04 22:26:48 -0700

    product-routes done

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
     try {
       const categoryData = await Category.findAll({
-        include: [{ model: Category }]
+        include: [{ model: Category },{model: Product}]
       });
       res.status(200).json(categoryData);
     }catch (err) {
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       
-      include: [{ model: Category }],
+      include: [{ model: Category },{model: Product}],
     });
 
     if (!categoryData) {
